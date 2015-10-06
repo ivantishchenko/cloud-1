839ee852368cf361c52e7fba2c22c861892dce9c
ivantishchenko <johnny.tishchenko@gmail.com>
2015-10-07 00:02:39 +0300

    configuring views with Hanndle bars

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+//var userService = require('../services/event-service');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+// GET /users/view
 router.get('/create', function(req, res, next) {
-  res.render('events', { title: 'Event creation' });
+  res.render('events/create');
 });
 
 module.exports = router;
--- services/event-service.js
+++ services/event-service.js
@@ -3,7 +3,7 @@ var Event = require('../models/event').Event;
 
 exports.addEvent = function(event, next) {
   var newEvent = new Event({
-    eventName: String: event.eventName,
+    eventName :String: event.eventName,
     eventPlace: String: event.eventPlace,
     eventDate: String: event.eventDate,
     eventWith: Array: event.eventWith
